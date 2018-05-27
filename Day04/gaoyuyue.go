fd38d19823c981a268575aa3f0b9b281118c896d
高语越 <gaoyuyue@outlook.com>
2018-05-27 10:57:58 +0800

    更新

--- Day04/gaoyuyue.go
+++ Day04/gaoyuyue.go
@@ -2,57 +2,43 @@ package main
 
 import (
 	"sort"
-	"container/list"
 	"fmt"
 )
 
 func tuple3(a []int) [][]int {
-	l := list.New()
+	nm := map[string]bool{}
 	n := len(a)
 	sort.Ints(a)
 	var r [][]int
-	for i,j:=0,n-1; i < j;{
-		var k int
-		for k=i+1; k<j; k++ {
-			if a[i] + a[j] + a[k] == 0 {
-				len := l.Len()
-				if len == 0 {
-					row := []int{a[i],a[j],a[k]}
-					r = append(r, row)
-					nm := map[int]bool{
-						i: true,
-						j: true,
-						k: true,
+	for i:=0; i<n; i++ {
+		for j:=i+1; j<n; j++ {
+			start := j+1
+			end := n-1
+			for start <= end {
+				middle := (start + end) / 2
+				sum := a[i] + a[j] + a[middle]
+				if sum == 0 {
+					len := len(nm)
+					row := []int{a[i],a[j],a[middle]}
+					sort.Ints(row)
+					key := fmt.Sprint(row)
+					if len == 0 || !nm[key]{
+						r = append(r, row)
+						nm[key] = true
 					}
-					l.PushBack(nm)
+					break
+				} else if sum > 0 {
+					end -= 1
 				} else {
-					for e,h:= l.Front(),0; h<len; e,h = e.Next(),h+1 {
-						v := e.Value.(map[int]bool)
-						if !v[a[i]] || !v[a[j]] || !v[a[k]] {
-							row := []int{a[i],a[j],a[k]}
-							r = append(r, row)
-							nm := map[int]bool{
-								i: true,
-								j: true,
-								k: true,
-							}
-							l.PushBack(nm)
-						}
-					}
+					start += 1
 				}
-				break
 			}
-		}
-		if a[i] + a[j] + a[k-1] <= 0  {
-			i++
-		} else {
-			j--
 		}
 	}
 	return r
 }
 
 func main() {
-	a := []int{-1, 0, 1, 2, -1, -4}
+	a := []int{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6}
 	fmt.Println(tuple3(a))
 }
