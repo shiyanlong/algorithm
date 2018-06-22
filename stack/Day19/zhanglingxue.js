b3b4ed0e7016b9026c208401a9dbd60a74069665
zhanglingxue <1050080567@qq.com>
2018-06-22 21:22:28 +0800

    迭代

--- stack/Day19/zhanglingxue.js
+++ stack/Day19/zhanglingxue.js
@@ -7,32 +7,85 @@
    3   6
   / \
  4   5
-输出: [1,2,3,4,5,6]*///问题在于判断不了空树！！
+输出: [1,2,3,4,5,6]*/
 var TreeNode = function(val) {
     this.val = val;
     this.left = null;
     this.right = null;
 };
 var root = new TreeNode(1);
+var r1 = new TreeNode(7);
 var r2 = new TreeNode(2);
 var r3 = new TreeNode(3);
 var r4 = new TreeNode(4);
 var r5 = new TreeNode(5);
 var r6 = new TreeNode(6);
 root.right = r2;
+// root.left = r1;
 r2.left = r3;
 r2.right = r6;
 r3.left = r4;
 r3.right = r5;
-var arr = [];
-function Tree(root) {
-    if(root === null){
-        return ;
-    }
-    if(root){
-        arr.push(root.val);
-        Tree(root.left);
-        Tree(root.right);
+// function Tree(root) {
+//     if(root === null){
+//         return arr;
+//     }
+//     math(root,arr);
+//     return arr;
+// }
+// function math(root,arr){
+//     arr.push(root.val);
+//     if(root.left !== null){
+//         math(root.left,arr);
+//     }
+//     if(root.right !== null){
+//         math(root.right,arr);
+//     }
+// }
+function Tree(root){
+    var stack = [];
+    var arr = [];
+    var pre = null;
+    var top = null;
+    var index = root;
+    while(index !== null||stack.length !== 0)
+    {
+        //先序迭代
+        while(index !== null)
+        {
+            arr.push(index.val);
+            stack.push(index);
+            index = index.left;
+        }
+        if(stack.length !== 0)
+        {
+            index=stack.pop();
+            index = index.right;
+        }
+        //中序迭代
+        while(index !== null){
+            stack.push(index);
+            index = index.left;
+        }
+        if(stack.length !== 0){
+            index = stack.pop();
+            arr.push(index.val);
+            index = index.right;
+        }
+        //后序迭代
+        while(index !== null){
+            stack.push(index);
+            index = index.left;
+        }
+        index = stack[stack.length - 1];
+        if(index.right === null || index.right === pre){
+            arr.push(index.val);
+            pre = index;
+            index = null;
+            stack.pop();
+        }else{
+            index = index.right;
+        }
     }
     return arr;
 }
