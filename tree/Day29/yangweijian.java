948cd6d5530b172f86ce74df75f1415b5e251b86
2391651936@qq.com <2391651936@qq.com>
2018-06-26 16:20:54 +0800

    Day 29

---
+++ tree/Day29/yangweijian.java
@@ -0,0 +1,20 @@
+public class Day29 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Integer.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    // =======================================================================
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return p == q;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
